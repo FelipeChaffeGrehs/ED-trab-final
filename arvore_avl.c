48a69207b0e9273dd99782f7eb0c0763dcef29aa
FelipeChaffeGrehs <fegrehs@gmail.com>
2026-07-05 22:15:09 -0300

    feature: nsei

--- arvore_avl.c
+++ arvore_avl.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "Estruturas.h"
+#include "estruturas.h"
 
 /* ======================= FUNÇÕES AUXILIARES ======================= */
 
